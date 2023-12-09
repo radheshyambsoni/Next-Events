22288f1b4d4c1005c32d9790929feb5fe21e6625
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-10 02:25:13 +0530

    chore: added :lipstick: icons to event-item component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,8 @@
-import Link from "next/link";
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
 	const { title, image, date, location, id } = props;
@@ -17,18 +19,26 @@ const EventItem = (props) => {
 	return (
 		<li className={classes.item}>
 			<img src={"/" + image} alt="" />
-			<div>
-				<div className={classes.content}>
+
+			<div className={classes.content}>
+				<div className={classes.summary}>
 					<h2>{title}</h2>
-					<div>
-						<time className={classes.date}>{humanReadableDate}</time>
+					<div className={classes.date}>
+						<DateIcon />
+						<time>{humanReadableDate}</time>
 					</div>
-					<div>
-						<address className={classes.address}>{formattedAddress}</address>
+					<div className={classes.address}>
+						<AddressIcon />
+						<address>{formattedAddress}</address>
 					</div>
 				</div>
 				<div className={classes.actions}>
-					<Button link={exploreLink}>Explore Event</Button>
+					<Button link={exploreLink}>
+						<span>Explore Event</span>
+						<span className={classes.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
