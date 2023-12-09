2b09cc655e9dbb82ec2791f0b30c569d1cc5661d
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-10 01:21:14 +0530

    feat: added :sparkles: event-list n event-item components

---
+++ components/events/event-item.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import classes from "./event-item.module.css";
+
+const EventItem = (props) => {
+	const { title, image, date, location, id } = props;
+
+	const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+
+	const formattedAddress = location.replace(", ", "\n");
+	const exploreLink = `/events/${id}`;
+
+	return (
+		<li className={classes.item}>
+			<img src={"/" + image} alt="" />
+			<div>
+				<div className={classes.content}>
+					<h2>{title}</h2>
+					<div>
+						<time className={classes.date}>{humanReadableDate}</time>
+					</div>
+					<div>
+						<address className={classes.address}>ADDRESS</address>
+					</div>
+				</div>
+				<div className={classes.actions}>
+					<Link href={exploreLink}>Explore Event</Link>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export default EventItem;
---
+++ components/events/event-list.js
@@ -0,0 +1,23 @@
+import EventItem from "./event-item";
+import classes from "./event-list.module.css";
+
+const EventList = (props) => {
+	const { items } = props;
+
+	return (
+		<ul className={classes.list}>
+			{items.map((event) => (
+				<EventItem
+					key={event.id}
+					id={event.id}
+					date={event.date}
+					title={event.title}
+					location={event.location}
+					image={event.image}
+				/>
+			))}
+		</ul>
+	);
+};
+
+export default EventList;
