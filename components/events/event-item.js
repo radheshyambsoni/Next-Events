d741b5e722a204e768b3ba2b306eb1acfc161aca
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-10 02:06:53 +0530

    feat: added :sparkles: button ui component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./event-item.module.css";
+import Button from "../ui/button";
 
 const EventItem = (props) => {
 	const { title, image, date, location, id } = props;
@@ -27,7 +28,7 @@ const EventItem = (props) => {
 					</div>
 				</div>
 				<div className={classes.actions}>
-					<Link href={exploreLink}>Explore Event</Link>
+					<Button link={exploreLink}>Explore Event</Button>
 				</div>
 			</div>
 		</li>
