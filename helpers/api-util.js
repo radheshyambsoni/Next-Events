f7cd1c1f5251a68b5959164a15c3e0a830939d14
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-14 09:31:50 +0530

    feat: added :sparkles: static generation to home page

---
+++ helpers/api-util.js
@@ -0,0 +1,21 @@
+export const getAllEvents = async () => {
+	const response = await fetch(
+		"https://next-events-29f94-default-rtdb.firebaseio.com/events.json"
+	);
+	const data = await response.json();
+
+	const events = [];
+	for (const key in data) {
+		events.push({
+			id: key,
+			...data[key],
+		});
+	}
+
+	return events;
+};
+
+export const getFeaturedEvents = async () => {
+	const allEvents = await getAllEvents();
+	return allEvents.filter((event) => event.isFeatured);
+};
