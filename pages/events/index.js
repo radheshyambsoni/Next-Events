b1f48d365b080730818315f36420d833bf3d0eff
Radheshyam Soni <radheshyambsoni@gmail.com>
2023-12-14 11:46:23 +0530

    feat: optimised :sparkles: all events page with ISR

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "@/helpers/api-util";
 import { useRouter } from "next/router";
 
-const AllEventsPage = () => {
-	const events = getAllEvents();
+const AllEventsPage = (props) => {
 	const router = useRouter();
+	const { events } = props;
 
 	const filterEventsHandler = (year, month) => {
 		const fullPath = `/events/${year}/${month}`;
@@ -19,6 +19,17 @@ const AllEventsPage = () => {
 			<EventList items={events} />
 		</>
 	);
+};
+
+export const getStaticProps = async () => {
+	const events = await getAllEvents();
+
+	return {
+		props: {
+			events: events,
+		},
+		revalidate: 60,
+	};
 };
 
 export default AllEventsPage;
